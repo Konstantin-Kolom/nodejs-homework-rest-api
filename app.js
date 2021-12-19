de3cd1e318f8308d52fd46883a5c0cfd61b1762a
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-12-19 20:24:10 +0200

    fix2

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('publiс'));
-app.use(express.static('temp'));
 
 app.use('/api/users', usersRouter);
 app.use('/api/contacts', contactsRouter);
